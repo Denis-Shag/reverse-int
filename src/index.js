8d7c8ae4b6f492c5f0faf79f45a9cf1278e0f9a4
Denis-Shag <130778866+Denis-Shag@users.noreply.github.com>
2023-07-31 18:38:20 +0500

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
+  module.exports = function reverse (n) {
+const isNegative = n < 0;
+  const numStr = String(isNegative ? -n : n);
+  const reversedStr = numStr.split('').reverse().join('');
+  const reversedNum = Number(reversedStr);
   
+  return reversedNum;
 }
